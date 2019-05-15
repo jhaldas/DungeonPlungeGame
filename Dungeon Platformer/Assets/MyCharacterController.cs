5cad40c7d17ebedfaaf4abadb24d337e46794b99
Jonny Haldas <jonathan.haldas@gmail.com>
2019-05-15 13:02:38 -0400

    Unity Project Upload
    Initial project upload.

---
+++ Dungeon Platformer/Assets/MyCharacterController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour
+{
+
+    private Collider2D playerCollider;
+	private Rigidbody2D rb;
+	private bool facingRight = true;
+
+	[SerializeField] private float m_JumpForce = 400f;
+
+	private Vector3 m_Velocity = Vector3.zero;
+
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+		//playerCollider = GetComponent<Collider2D>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+
+	public void Move(float move){
+		Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+
+		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+		if (move > 0 && !facingRight)
+		{
+			// ... flip the player.
+			Flip();
+		}
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if (move < 0 && facingRight)
+		{
+			// ... flip the player.
+			Flip();
+		}
+
+
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Dungeon Platformer/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public MyCharacterController controller;
+	public float runSpeed = 40f;
+
+	float hMovement = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+		Debug.Log(hMovement);
+	}
+
+	void FixedUpdate(){
+		controller.Move(hMovement * Time.fixedDeltaTime) ;
+	}
+}

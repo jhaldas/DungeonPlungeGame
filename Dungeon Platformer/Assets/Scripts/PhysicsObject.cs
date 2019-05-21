b34198ef2eea845c49163243c1a14afcd6200436
Jonny Haldas <jonathan.haldas@gmail.com>
2019-05-21 16:15:18 -0400

    Improved movement

--- Dungeon Platformer/Assets/Scripts/PhysicsObject.cs
+++ Dungeon Platformer/Assets/Scripts/PhysicsObject.cs
@@ -36,8 +36,13 @@ public class PhysicsObject: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		targetVelocity = Vector2.zero;   
+		ComputeVelocity();
     }
+
+	protected virtual void ComputeVelocity(){
+	
+	}
 
 	void FixedUpdate(){
 		velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
--- Dungeon Platformer/Assets/Scripts/PlayerController.cs
+++ Dungeon Platformer/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class PlayerController : PhysicsObject
 {
+
+	public float jumpTakeOffSpeed = 7;
+	public float maxSpeed = 7;
+
+	private bool facingRight = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    protected override void ComputeVelocity(){
+		Vector2 move = Vector2.zero;
 
-		if(Input.GetButtonDown("Left")){
-			targetVelocity = Vector2.left;   
-		}else if(Input.GetButtonDown("Right")){
-			targetVelocity = Vector2.right;   
+		move.x = Input.GetAxis("Horizontal");
+
+		if(move.x > 0 && !facingRight){
+			Flip();
+			facingRight = true;
+
+		}else if(move.x < 0 && facingRight){
+			Flip();
+			facingRight = false;
 		}
 
-		//targetVelocity = Vector2.left;   
-    }
+
+		if(Input.GetButtonDown("Jump") && grounded){
+			velocity.y = jumpTakeOffSpeed;
+		}
+		else if (Input.GetButtonUp("Jump")){
+			if(velocity.y > 0){
+				velocity.y = velocity.y *.5f;
+			}
+		}
+
+		targetVelocity = move * maxSpeed;
+
+	}
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+
+		transform.Rotate(0f, 180f, 0f);
+	}
 }
